4c710e57b6f6b2eb5f98cc42ddddb8355b1e6e1f
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-17 21:50:51 +0530

    encoder_rtl.v

--- encoder/encoder_rtl.v
+++ encoder/encoder_rtl.v
@@ -3,7 +3,7 @@ module encoder_4_2(i,en,y);
   input en;
   output [1:0]y;
 
-  assign y[0] = en & i[1] & i[3];
-  assign y[1] = en & i[2] & i[3];
+  assign y[0] = en & (i[1] | i[3]);
+  assign y[1] = en & (i[2] | i[3]);
   
 endmodule
