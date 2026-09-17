6b1e772d3dc9f4b7ae4ba59bcaf184083057f61b
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-17 21:59:04 +0530

    encoder_tb.v

--- encoder/encoder_tb.v
+++ encoder/encoder_tb.v
@@ -6,7 +6,7 @@ module tb;
   encoder_4_2 abc(i,en,y);
 
   initial begin
-    $monitor("Time = %0t, en = %b; i = %b; y = %b", $time, en, i, y);
+    $monitor("Time = %0t, en = %b; i[0] = %b; i[1] = %b; i[2] = %b; i[3] = %b;  y = %b", $time, en, i[0], i[1], i[2], i[3], y);
     for( int k = 0; k<32; k++)
       begin
         {en, i} = k;
